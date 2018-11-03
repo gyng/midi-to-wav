c09b3b0602f4a7cdc5a339edda0ba7a65ba632d6
gyng <ng.guoyou+git@gmail.com>
2018-11-03 13:11:58 +0800

    Update to latest synthrs

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,28 @@ extern crate synthrs;
 
 use std::env;
 
-use synthrs::synthesizer::{ make_samples_from_midi, quantize_samples };
+use synthrs::synthesizer::{make_samples_from_midi_file, quantize_samples};
+use synthrs::wave;
 use synthrs::writer::write_wav;
 
-fn main() {
+fn main() -> Result<(), synthrs::errors::SynthrsError> {
     let args: Vec<String> = env::args().collect();
     assert!(args.len() == 3);
 
     let input_path = &args[1][..];
     let output_path = &args[2][..];
 
-    write_wav(output_path, 44100,
-        quantize_samples::<i16>(
-            make_samples_from_midi(44100, input_path)
-        )
-    ).ok().expect("failed to synthesize MIDI file");
+    write_wav(
+        output_path,
+        44100,
+        &quantize_samples::<i16>(&make_samples_from_midi_file(
+            wave::square_wave,
+            44100,
+            true,
+            input_path,
+        )?),
+    ).ok()
+    .expect("failed to synthesize MIDI file");
+
+    Ok(())
 }
